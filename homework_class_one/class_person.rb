a85f660349f4c420e87b54c50ab87e6f1dead92d
RubyBossKirill <kirill_richardson@mail.ru>
2024-03-19 19:29:59 +0500

    method calculate_birth_year

--- homework_class_one/class_person.rb
+++ homework_class_one/class_person.rb
@@ -1,3 +1,6 @@
+require 'date'
+require 'time'
+
 class Person
     def initialize(name, age)
         @name = name
@@ -7,12 +10,23 @@ class Person
     def age
         return p 'Age is confidential'
     end
+
+    def calculate_birth_year(str, format="%d.%m.%Y")
+        yearUser = Date.strptime(str,'%d.%m.%Y').strftime('%Y')
+        date = Time.now.strftime("%Y")
+        person_age = date.to_i - yearUser.to_i
+        p person_age
+    end
 end
 
 p 'Напишите ваше имя'
 userName = gets.chomp
 p 'Напиши свой возраст'
 userAge = gets.chomp
+p 'Напиши дату рождения в формате ДД.ММ.ГГГГ'
+userDate = gets.chomp
+
 
 person = Person.new(userName, userAge)
 person.age
+person.calculate_birth_year(userDate)
