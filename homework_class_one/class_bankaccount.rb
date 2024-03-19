f6be4b62abf81658b453319b03854b9118233e7e
RubyBossKirill <kirill_richardson@mail.ru>
2024-03-19 22:00:37 +0500

    Implement a method withdraw(amount) in the BankAccount class that subtracts the specified amount from the balance, but only if the balance is sufficient.

--- homework_class_one/class_bankaccount.rb
+++ homework_class_one/class_bankaccount.rb
@@ -1,20 +1,34 @@
 class BankAccount
     attr_reader :account_number
     def initialize(balance, account_number)
-        @balance = balance.to_i
-        @account_number = account_number.to_i
+        @balance = balance
+        @account_number = account_number
     end
 
     def deposite(sum)
-        @balance = @balance + sum.to_i
+        @balance = @balance + sum
+    end
+    
+    def withdraw(amount)
+        if (@balance - amount) >= 0
+            @balance = @balance - amount
+            p "Ваш баланс #{@balance}"
+        else
+            p 'Ваш баланс не достаточен для совершение операции'
+        end
     end
 end
 
 p 'Введи сумму'
-summa = gets.chomp
+summa = gets.chomp.to_i
 
 account = BankAccount.new(summa, 1)
 p 'Введи сколько прибавить к сумме'
-count = gets.chomp
+count = gets.chomp.to_i
 
 p account.deposite(count)
+
+p 'Введите сколько отнять от баланса'
+count = gets.chomp.to_i
+
+account.withdraw(count)
