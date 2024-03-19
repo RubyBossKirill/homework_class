6df01b59dffcb1f429c0b5b1587732af704e8fa6
RubyBossKirill <kirill_richardson@mail.ru>
2024-03-19 19:01:15 +0500

    первый абзац домашнего задания

--- homework_class_one/class_car.rb
+++ homework_class_one/class_car.rb
@@ -1,2 +1,9 @@
 class Car
+    attr_reader :make, :model, :year
+    attr_writer :make, :model, :year
+    def initialize(make, model, year)
+        @make = make
+        @model = model
+        @year = year
+    end
 end
