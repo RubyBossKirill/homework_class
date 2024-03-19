f661b1ce7d36a191239f65fa6d4a3ea2e152bcf9
RubyBossKirill <kirill_richardson@mail.ru>
2024-03-19 21:55:05 +0500

    Implement a method deposit(amount) in the BankAccount class that adds the specified amount to the balance.

--- homework_class_one/class_bankaccount.rb
+++ homework_class_one/class_bankaccount.rb
@@ -1,7 +1,20 @@
 class BankAccount
     attr_reader :account_number
     def initialize(balance, account_number)
-        @balance = balance
-        @account_number = account_number
+        @balance = balance.to_i
+        @account_number = account_number.to_i
+    end
+
+    def deposite(sum)
+        @balance = @balance + sum.to_i
     end
 end
+
+p 'Введи сумму'
+summa = gets.chomp
+
+account = BankAccount.new(summa, 1)
+p 'Введи сколько прибавить к сумме'
+count = gets.chomp
+
+p account.deposite(count)
